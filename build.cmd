2e0e17e6e970b0a2c3d81c09f0ce1ea889533789
MysticUser <ethiles@online.no>
2020-08-28 13:51:45 +0200

    Environment fix for windows

--- build.cmd
+++ build.cmd
@@ -3,3 +3,5 @@ docker build -t=test .
 docker create -ti --name dummy test bash
 docker cp dummy:/tmp/target ./
 docker rm -f dummy
+
+pause
--- runapp.cmd
+++ runapp.cmd
@@ -1,6 +1,8 @@
 
 :: START MARIADB
-docker run --rm --name mariadb -p 3306:3306/tcp -v database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=eplepai -d mariadb
+docker run --rm --name mariadb -p 3306:3306/tcp -v %cd%\database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=eplepai -d mariadb
 
 :: START PAYARA
-docker run --rm --name payara -p 8080:8080 -p 4848:4848 -v ./target:/opt/payara/deployments -d payara/server-full
+docker run --rm --name payara -p 8080:8080 -p 4848:4848 -v %cd%\target:/opt/payara/deployments -d payara/server-full
+
+pause
