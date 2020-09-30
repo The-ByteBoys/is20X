9be057b77c404bf40cdb244664f96887535a0841
Johannes <54539681+shiftywitch@users.noreply.github.com>
2020-09-30 13:18:28 +0200

    Johannes dev (#1)
    * added apache.poi dependency
    
    * added an example for a excelreader
    
    * gitTest
    
    * deleted test
    
    * functionality to use any excelsheet
    
    * Added excelDocuments
    
    * added a choose Document function.
    
    * new getRowValues() for getting values from one row, new keyGenerator() for generating keys for the Arraylist in getRowValues(), new name: ReadExcel.java -> ExcelReader.java
    
    * Example and readme for the ExcelReader
    
    * now generates keys for all classes, ignores the 3 first rows in a document: Rank 1 is index 0, throws two exceptions in chooseDocument()
    
    * documentation
    
    * junit dependency for testing
    
    * Documentation
    
    * example

---
+++ src/main/java/tools/excel/example.java
@@ -0,0 +1,18 @@
+package tools.excel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class example {
+    public static void main(String[] args) {
+
+        ExcelReader er = new ExcelReader();
+        er.chooseDocument(18,5);
+        HashMap<String, Object> mylist = er.getRowValues(4);
+
+        for (Map.Entry<String, Object> set : mylist.entrySet()) {
+            System.out.println(set.getKey() + " = " + set.getValue());
+        }
+    }
+
+}
