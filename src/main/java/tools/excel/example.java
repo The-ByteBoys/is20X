2b9bb02159ab4fe7c2f0f9d2504e42ff0ace9963
Johannes <johbirk00@gmail.com>
2020-09-30 13:14:53 +0200

    example

--- src/main/java/tools/excel/example.java
+++ src/main/java/tools/excel/example.java
@@ -7,11 +7,12 @@ public class example {
     public static void main(String[] args) {
 
         ExcelReader er = new ExcelReader();
-        er.chooseDocument(5,5);
+        er.chooseDocument(18,5);
         HashMap<String, Object> mylist = er.getRowValues(4);
 
         for (Map.Entry<String, Object> set : mylist.entrySet()) {
             System.out.println(set.getKey() + " = " + set.getValue());
         }
     }
+
 }
