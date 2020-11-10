d2a9584cb21deddf29c4ed998573a4824cbe3e55
Eirik <ethiles@online.no>
2020-11-10 11:16:44 +0100

    README update, and windows fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# Variables
+mysqlRootPassword=eplepai
+
+# Database Variables
+host=172.17.0.1
+port=3306
+bruker=roing
+passord=Passord123
+databaseNavn=roro
+
+
 # Start database
-docker run --rm --name mariadb -p 3306:3306/tcp -v "$(pwd)/database":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=eplepai -d mariadb:10.5.5
+docker run --rm --name mariadb -p $port\:3306/tcp -v "$(pwd)/database":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlRootPassword -d mariadb:10.5.5
 
 # Start maven-cache
 docker volume create --name maven-repo
@@ -12,13 +23,6 @@ docker run --rm --name payara -p 8080:8080 -p 4848:4848 -d nosp/web-app-skeleton
 sleep 5
 echo ""
 ./build.sh
-
-# Database Variables
-host=172.17.0.1
-port=3306
-bruker=roing
-passord=Passord123
-databaseNavn=roro
 
 # Setup database connection
 docker exec -it payara asadmin --user=admin --passwordFile /opt/payara/passwordFile create-jdbc-connection-pool --datasourceclassname org.mariadb.jdbc.MySQLDataSource --restype javax.sql.XADataSource --property serverName=$host\:portNumber=$port\:user=$bruker\:password=$passord\:databaseName=$databaseNavn mariapool
